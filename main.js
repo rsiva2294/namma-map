5fd9aa79567b60f0f0fd1048d620b45a8dc86f3c
rsiva2294 <rsiva2294@gmail.com>
2026-04-15 17:56:24 +0530

    Switch to high-contrast Light Mode with Voyager tiles

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function initMap() {
         zoomControl: false
     }).setView([11.1271, 78.6569], 7);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 20
