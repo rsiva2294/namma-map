3d450c43727d7715552cc1ea837088540a14a57c
rsiva2294 <rsiva2294@gmail.com>
2026-04-15 17:45:23 +0530

    Improve UI visibility with dark map tiles and high-contrast glassmorphism

--- main.js
+++ main.js
@@ -22,8 +22,10 @@ function initMap() {
         zoomControl: false
     }).setView([11.1271, 78.6569], 7);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(map);
 
     L.control.zoom({
